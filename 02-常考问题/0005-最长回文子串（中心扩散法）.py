6777c5d10a4e77e6a2443344f2c7fab016c39503
李威 <121088825@qq.com>
2019-07-30 10:56:13 +0800

    update 5.

--- 02-常考问题/0005-最长回文子串（中心扩散法）.py
+++ 02-常考问题/0005-最长回文子串（中心扩散法）.py
@@ -3,52 +3,43 @@
 
 class Solution:
     def longestPalindrome(self, s):
-        """
-        最长回文子串，比较容易想到的就是中心扩散法
-        :type s: str
-        :rtype: str
-        """
+        # 中心扩散法
         size = len(s)
         if size == 0:
             return ''
 
-        # 至少就是 1
+        # 最短回文串的长度至少是 1
         longest_palindrome = 1
-
         longest_palindrome_str = s[0]
 
         for i in range(size):
-            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
-            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
+
+            palindrome_odd_str, odd_len = self.__center_spread(s, size, i, i)
+            palindrome_even_str, even_len = self.__center_spread(s, size, i, i + 1)
 
             # 当前找到的最长回文子串
-            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
+            cur_max_sub = palindrome_odd_str if odd_len >= even_len else palindrome_even_str
             if len(cur_max_sub) > longest_palindrome:
                 longest_palindrome = len(cur_max_sub)
                 longest_palindrome_str = cur_max_sub
 
         return longest_palindrome_str
 
-    def __center_spread(self, s, size, left, right):
-        """
-        left = right 的时候，表示回文中心是一条线，回文串的长度是奇数
-        right = left + 1 的时候，表示回文中心是任意一个字符，回文串的长度是偶数
-        :param s:
-        :param size:
-        :param left:
-        :param right:
-        :return:
-        """
-        l = left
-        r = right
+    def __center_spread(self, s, size, index_left, index_right):
+        # left = right 的时候，表示回文中心是一条线，回文串的长度是奇数
+        # right = left + 1 的时候，表示回文中心是任意一个字符，回文串的长度是偶数
+        # 返回当前得到的回文子串和回文子串的长度
+        left = index_left
+        right = index_right
 
-        while l >= 0 and r < size and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return s[l + 1:r], r - l - 1
+        while left >= 0 and right < size and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1:right], right - left - 1
 
 
 if __name__ == '__main__':
     solution = Solution()
-    longestPalindrome = solution.longestPalindrome("babad")
+    s = "babad"
+    longestPalindrome = solution.longestPalindrome(s)
     print(longestPalindrome)
