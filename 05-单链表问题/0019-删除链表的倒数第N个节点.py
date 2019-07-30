5ea4f9bb6c7c20cdc970adf51e34807329e0dd74
李威 <121088825@qq.com>
2019-07-30 11:28:55 +0800

    add 19.

--- 05-单链表问题/0019-删除链表的倒数第N个节点.py
+++ 05-单链表问题/0019-删除链表的倒数第N个节点.py
@@ -1,8 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        pass
