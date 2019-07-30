7308f8443fe183be2888a7f5004863d6c8990a10
李威 <121088825@qq.com>
2019-07-30 11:24:39 +0800

    update 13.

--- 21-字符串/0013-罗马数字转整数.py
+++ 21-字符串/0013-罗马数字转整数.py
@@ -1,13 +1,9 @@
 # 13. 罗马数字转整数
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def romanToInt(self, s: str) -> int:
 
-        l = len(s)
-        if l == 0:
+        size = len(s)
+        if size == 0:
             return 0
         map = {
             'I': 1,
@@ -20,9 +16,12 @@ class Solution(object):
         }
 
         res = map[s[0]]
-        for i in range(1, l):
+        for i in range(1, size):
+
             pre = map[s[i - 1]]
             cur = map[s[i]]
+            # 这是唯一的一种需要注意的特殊情况，当前面的数字比当前数字要小的时候
+            # 要用：当前数字 - 前面数字，因为前面数字加过了，所以要减去 2 倍
             if pre < cur:
                 res += (cur - 2 * pre)
             else:
