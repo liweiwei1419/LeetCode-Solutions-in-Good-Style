640ec786d9f5b095ef4adb6949cee7ffaeebebef
李威 <121088825@qq.com>
2019-08-26 12:29:28 +0800

    modify folder name.

--- 03-数组问题/0344-反转字符串.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def reverseString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        return s[::-1]
--- 15-树状数组/Test.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    arr = [10, 15, 17, 19, 20, 14, 12]
-
-    arr = [1, 2, 3, 4, 5, 6, 7]
-    cumsum = [0] * len(arr)
-    cumsum[0] = arr[0]
-    for i in range(1, len(arr)):
-        cumsum[i] = cumsum[i - 1] + arr[i]
-    print(cumsum)
