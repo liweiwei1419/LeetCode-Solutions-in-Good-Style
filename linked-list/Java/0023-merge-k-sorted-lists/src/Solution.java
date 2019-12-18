13364bee56baa45a3a2cb4c4847f14df1e74b57d
李威 <121088825@qq.com>
2019-12-19 01:56:54 +0800

    添加了并查集的代码。

--- linked-list/Java/0023-merge-k-sorted-lists/src/Solution.java
+++ linked-list/Java/0023-merge-k-sorted-lists/src/Solution.java
@@ -41,24 +41,46 @@ public class Solution {
         if (len == 0) {
             return null;
         }
-        Comparator<ListNode> comparator = Comparator.comparingInt(a -> a.val);
-        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(comparator);
+
+        // 默认是最小堆
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(len, Comparator.comparingInt(a -> a.val));
         for (int i = 0; i < len; i++) {
             // 注意：这里要注意到测试用例中，ListNode 为 null 的特殊情况，空节点是一定不能放入优先队列的，把空节点放入优先队列没有意义
             if (lists[i] != null) {
-                priorityQueue.add(lists[i]);
+                minHeap.add(lists[i]);
             }
         }
+
         ListNode dummyNode = new ListNode(-1);
-        ListNode cur = dummyNode;
-        while (!priorityQueue.isEmpty()) {
-            ListNode curMin = priorityQueue.poll();
-            cur.next = curMin;
-            cur = cur.next;
-            if (curMin.next != null) {
-                priorityQueue.add(curMin.next);
+        ListNode curNode = dummyNode;
+        while (!minHeap.isEmpty()) {
+            ListNode top = minHeap.poll();
+            curNode.next = top;
+            curNode = curNode.next;
+
+            if (top.next != null) {
+                minHeap.offer(top.next);
             }
         }
         return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums1 = {1, 4, 5};
+        Integer[] nums2 = {1, 3, 4};
+        Integer[] nums3 = {2, 6};
+
+        ListNode head1 = new ListNode(nums1);
+        ListNode head2 = new ListNode(nums2);
+        ListNode head3 = new ListNode(nums3);
+
+        ListNode[] lists = new ListNode[3];
+        lists[0] = head1;
+        lists[1] = head2;
+        lists[2] = head3;
+
+        Solution solution = new Solution();
+        ListNode res = solution.mergeKLists(lists);
+        System.out.println(res);
     }
 }
