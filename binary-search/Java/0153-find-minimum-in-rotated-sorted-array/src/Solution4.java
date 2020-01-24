00d406802da5cb4abb5c5158010ee9dd62d3c44c
李威 <121088825@qq.com>
2020-01-24 15:14:07 +0800

    添加了二分法经典问题。

--- binary-search/Java/0153-find-minimum-in-rotated-sorted-array/src/Solution4.java
+++ binary-search/Java/0153-find-minimum-in-rotated-sorted-array/src/Solution4.java
@@ -1,7 +1,5 @@
 public class Solution4 {
 
-    // 分治法
-
     public int findMin(int[] nums) {
         int len = nums.length;
         if (len == 0) {
@@ -11,20 +9,21 @@ public class Solution4 {
     }
 
     private int findMin(int[] nums, int left, int right) {
-        // 思考：这个临界条件是为什么?
-        // 或者写成 left + 1 >= right
-        if (left == right || left + 1 == right) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        if (left + 1 == right) {
             return Math.min(nums[left], nums[right]);
         }
         int mid = (left + right) >>> 1;
-        // 8 9 1 2 3 4 5 6 7
+
         if (nums[mid] < nums[right]) {
-            // 右边是顺序数组
+            // 右边是顺序数组，[mid + 1 , right] 这个区间里的元素可以不看
             return Math.min(findMin(nums, left, mid - 1), nums[mid]);
         } else {
-            // 左边是顺序数组
             // nums[mid] > nums[right]
-            // 3 4 5 6 7 8 1 2
+            // 左边是顺序数组，[left + 1, mid] 这个区间里的元素可以不看
             return Math.min(nums[left], findMin(nums, mid + 1, right));
         }
     }
