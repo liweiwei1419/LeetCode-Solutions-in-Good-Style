986ce07a88aac709d7ab265acec67626c9d5f704
liweiwei1419 <liweiwei1419@gmail.com>
2020-03-24 11:42:46 +0800

    添加了并查集的相关问题。

--- array/Java/0451-sort-characters-by-frequency/src/Solution2.java
+++ array/Java/0451-sort-characters-by-frequency/src/Solution2.java
@@ -14,10 +14,12 @@ public class Solution2 {
         Comparator<Character> comparator = new Comparator<Character>() {
             @Override
             public int compare(Character o1, Character o2) {
-                if(map.get(o2) - map.get(o1)==0){ // 要注意：如果出现频次相同，要按字母顺序排序， "loveleetcode" 就是一个很好的测试用例
+                if (map.get(o2) - map.get(o1) == 0) {
+                    // 要注意：如果出现频次相同，要按字母顺序排序， "loveleetcode" 就是一个很好的测试用例
                     return o1.compareTo(o2);
                 }
-                return map.get(o2) - map.get(o1); // 注意顺序
+                // 注意顺序
+                return map.get(o2) - map.get(o1);
             }
         };
         Character[] cArr = new Character[len];
