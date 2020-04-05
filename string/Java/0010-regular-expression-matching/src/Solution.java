89f78d375d8a501e6fe3459670deb2a6c8b1bae5
liweiwei1419 <liweiwei1419@gmail.com>
2020-04-05 18:33:56 +0800

    添加了两道字符串的问题。

--- string/Java/0010-regular-expression-matching/src/Solution.java
+++ string/Java/0010-regular-expression-matching/src/Solution.java
@@ -15,15 +15,18 @@ public class Solution {
             return s.isEmpty();
         }
 
+        char[] sCharArray = s.toCharArray();
+        char[] pCharArray = p.toCharArray();
+
         // 满足 firstMatch 就表示可以同时砍掉模式串 p 和文本串 s 的第 1 个字符
         // ① 第 1 个字符正正好相等
         // ② 模式串的第 1 个字符正正好是 '.'，因为可以匹配任意单个字符
-        boolean firstMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        boolean firstMatch = !s.isEmpty() && (sCharArray[0] == pCharArray[0] || pCharArray[0] == '.');
 
         // 当模式串的下一个是 *
         // ① * 号的作用是 0 次：模式串与前面的都不匹配，例如：s = 'bb'，p = 'a*bb'，模式串直接砍掉 2 个（连同 * 一起砍掉），继续匹配
         // ② * 号的作用是 1 次或者更多次：在满足 firstMatch 的前提下，s 砍掉 1 个然后继续
-        if (p.length() > 1 && p.charAt(1) == '*') {
+        if (p.length() > 1 && pCharArray[1] == '*') {
             if (isMatch(s, p.substring(2))) {
                 return true;
             }
