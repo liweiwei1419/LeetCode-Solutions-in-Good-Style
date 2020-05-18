8472c0295eb36938d439726fbfab467a161d150e
liweiwei1419 <liweiwei1419@gmail.com>
2020-05-18 13:45:07 +0800

    添加了位运算的相关问题。

--- bit-manipulation/Java/0136-single-number/src/Solution2.java
+++ bit-manipulation/Java/0136-single-number/src/Solution2.java
@@ -5,6 +5,7 @@ public class Solution2 {
         if (len == 0) {
             throw new RuntimeException("数组元素为空，没有只出现一次的数字");
         }
+        
         int res = nums[0];
         for (int i = 1; i < len; i++) {
             res ^= nums[i];
