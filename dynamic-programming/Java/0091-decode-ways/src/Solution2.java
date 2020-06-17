c229307d55869f5970f8d8de06c6525594cdd737
liweiwei1419 <liweiwei1419@gmail.com>
2020-06-17 13:42:38 +0800

    添加了动态规划的代码。

--- dynamic-programming/Java/0091-decode-ways/src/Solution2.java
+++ dynamic-programming/Java/0091-decode-ways/src/Solution2.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution2 {
 
     public int numDecodings(String s) {
         int len = s.length();
@@ -6,39 +6,35 @@ public class Solution {
             return 0;
         }
 
-        // dp[i] 以 s[i] 结尾的前缀子串有多少种解码方法
-        // dp[i] = dp[i - 1] * 1 if nums[i] != '0'
-        // dp[i] += dp[i - 2] * 1 if  10 <= int(nums[i - 1..i]) <= 26
-        int[] dp = new int[len];
-        dp[0] = 0;
+        // dp[i] 以 s[i - 1] 结尾的前缀子串有多少种解法方法
+        // dp[i] = dp[i - 1] * 1 if nums[i - 1] != '0'
+        // dp[i] += dp[i - 2] * 1 if  10 <= int(nums[i - 2..i - 1]) <= 26
 
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
         char[] charArray = s.toCharArray();
         if (charArray[0] == '0') {
             return 0;
         }
-        dp[0] = 1;
+        dp[1] = 1;
 
         for (int i = 1; i < len; i++) {
             if (charArray[i] != '0') {
-                dp[i] = dp[i - 1];
+                dp[i + 1] = dp[i];
             }
 
             int num = 10 * (charArray[i - 1] - '0') + (charArray[i] - '0');
             if (num >= 10 && num <= 26) {
-                if (i == 1) {
-                    dp[i]++;
-                } else {
-                    dp[i] += dp[i - 2];
-                }
+                dp[i + 1] += dp[i - 1];
             }
         }
-        return dp[len - 1];
+        return dp[len];
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution2 solution2 = new Solution2();
         String s = "10";
-        int res = solution.numDecodings(s);
+        int res = solution2.numDecodings(s);
         System.out.println(res);
     }
 }
