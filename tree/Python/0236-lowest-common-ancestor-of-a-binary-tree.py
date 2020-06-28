44a2f93db20681d1accb5c300002caa496328f67
liweiwei1419 <liweiwei1419@gmail.com>
2020-06-28 21:16:03 +0800

    添加了树的问题。

---
+++ tree/Python/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root is q or root is p:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        if left:
+            return left
+        return right
+
