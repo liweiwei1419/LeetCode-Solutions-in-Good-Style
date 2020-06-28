5802c2fe9915891aec026bd733de476fa60ea7ec
liweiwei1419 <liweiwei1419@gmail.com>
2020-06-28 21:16:26 +0800

    添加了一道前缀和的问题。

--- segment-tree/Python/0303-range-sum-query-immutable（前缀和数组实现）.py
+++ segment-tree/Python/0303-range-sum-query-immutable（前缀和数组实现）.py
@@ -9,16 +9,17 @@ class NumArray:
         :type nums: List[int]
         """
         self.size = len(nums)
+        if self.size == 0:
+            return
 
-        if self.size > 0:
-            self.cumsum = [0 for _ in range(self.size + 1)]
-            self.cumsum[1] = nums[0]
-            for i in range(2, len(nums) + 1):
-                self.cumsum[i] = self.cumsum[i - 1] + nums[i - 1]
+        self.pre_sum = [0 for _ in range(self.size + 1)]
+        self.pre_sum[0] = 0
+        for i in range(self.size):
+            self.pre_sum[i + 1] = self.pre_sum[i] + nums[i]
 
     def sumRange(self, i: int, j: int) -> int:
         if self.size > 0:
-            return self.cumsum[i] - self.cumsum[i - 1]
+            return self.pre_sum[j + 1] - self.pre_sum[i]
         return 0
 
 # Your NumArray object will be instantiated and called as such:
